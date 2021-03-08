62d67bc5e35810286b75762177052396734ce7dd
0dj3 <71371862+0dj3@users.noreply.github.com>
2021-03-08 14:19:56 +0900

    init

---
+++ Untitled.sol
@@ -0,0 +1,58 @@
+contract Test
+{
+    struct Owner
+    {
+        string name;
+        uint passSer;
+        uint passNum;
+        uint256 date;
+        string phoneNumber;
+    }
+    
+    struct Home
+    {
+        string homeAddress;
+        fixed area;
+        Owner owner;
+        uint cost;
+    }
+    
+    struct Request
+    {
+        int requestType;
+        Home home;
+        int result;
+    }
+    
+    struct Employee
+    {
+        string name;
+        string position;
+        string phoneNumber;
+    }
+    
+    Owner private owner;
+    Home private home;
+    Request private request;
+    Employee private employee;
+    
+    function SetRequest(int _requestType, string _homeAddress, fixed _area, uint _cost,  ) public
+    {
+        
+    }
+    
+    function Test1(string memory Message) public 
+    {
+        message = Message;
+    }
+    
+    function SetMessage(string memory newMessage) public
+    {
+        message = newMessage;
+    }
+    
+    function GetMessage() public returns (string memory)
+    {
+        return message;
+    }
+}
